9cc4707986ace7bfff4d1a233e2ff40f4492fe0d
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-09 14:48:37 -0200

    adds new produces

--- src/main/java/com/example/jnosql/redis/RedisProducer.java
+++ src/main/java/com/example/jnosql/redis/RedisProducer.java
@@ -10,6 +10,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
@@ -23,31 +24,36 @@ public class RedisProducer {
     private RedisBucketManagerFactory managerFactory;
 
     @PostConstruct
-    public void init () {
+    public void init() {
         configuration = new RedisConfiguration();
         managerFactory = configuration.get();
     }
 
     @Produces
-    public BucketManager getManager () {
+    public BucketManager getManager() {
         return managerFactory.getBucketManager(BUCKET);
     }
 
     @Produces
-    public List<String> getList(){
+    public List<String> getList() {
         return managerFactory.getList("list", String.class);
     }
 
     @Produces
-    public Set<String> getSet(){
+    public Set<String> getSet() {
         return managerFactory.getSet("queue", String.class);
     }
 
     @Produces
-    public Queue<String> getQueue(){
+    public Queue<String> getQueue() {
         return managerFactory.getQueue("queue", String.class);
     }
 
+    @Produces
+    public Map<String, String> getMap() {
+        return managerFactory.getMap("map", String.class, String.class);
+    }
+    
     @Produces
     public Counter getCounter() {
         return managerFactory.getCounter("counter");
