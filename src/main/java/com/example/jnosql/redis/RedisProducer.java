5f523a83aa307e94af75332a190fe6928c2d5f0c
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-09 14:37:22 -0200

    Adds new produces

--- src/main/java/com/example/jnosql/redis/RedisProducer.java
+++ src/main/java/com/example/jnosql/redis/RedisProducer.java
@@ -1,21 +1,26 @@
 package com.example.jnosql.redis;
 
+import org.jnosql.diana.api.key.BucketManager;
+import org.jnosql.diana.redis.key.Counter;
+import org.jnosql.diana.redis.key.RedisBucketManagerFactory;
+import org.jnosql.diana.redis.key.RedisConfiguration;
+import org.jnosql.diana.redis.key.SortedSet;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import org.jnosql.diana.api.key.BucketManager;
-import org.jnosql.diana.api.key.BucketManagerFactory;
-import org.jnosql.diana.api.key.KeyValueConfiguration;
-import org.jnosql.diana.redis.key.RedisConfiguration;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 @ApplicationScoped
 public class RedisProducer {
 
     private static final String BUCKET = "users";
 
-    private KeyValueConfiguration configuration;
+    private RedisConfiguration configuration;
 
-    private BucketManagerFactory managerFactory;
+    private RedisBucketManagerFactory managerFactory;
 
     @PostConstruct
     public void init () {
@@ -27,5 +32,31 @@ public class RedisProducer {
     public BucketManager getManager () {
         return managerFactory.getBucketManager(BUCKET);
     }
+
+    @Produces
+    public List<String> getList(){
+        return managerFactory.getList("productList", String.class);
+    }
+
+    @Produces
+    public Set<String> getSet(){
+        return managerFactory.getSet("productSet", String.class);
+    }
+
+    @Produces
+    public Queue<String> getQueue(){
+        return managerFactory.getQueue("queue", String.class);
+    }
+
+    @Produces
+    public Counter getCounter() {
+        return managerFactory.getCounter("counter");
+    }
+
+    @Produces
+    public SortedSet getSortedSet() {
+        return managerFactory.getSortedSet("soccer");
+    }
+
 
 }
