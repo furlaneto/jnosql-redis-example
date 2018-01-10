46179121f57e4b37273ab3b85cc7c612a8e5aca7
Otavio Santana <otaviopolianasantana@gmail.com>
2018-01-10 07:02:24 -0200

    creates counter sample

--- src/main/java/com/example/jnosql/redis/Main8.java
+++ src/main/java/com/example/jnosql/redis/Main8.java
@@ -6,7 +6,7 @@ import org.jnosql.diana.redis.key.Counter;
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 
-public class Main6 {
+public class Main8 {
 
     public static void main (String[] args) {
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
